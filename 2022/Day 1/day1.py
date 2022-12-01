6791cdc05d8ae4f9a55850b2e7ee7c50b4d5703b
Ivan <ivan@ivanryan.dev>
2022-12-01 15:50:27 +0900

    Day 1: python solution

---
+++ 2022/Day 1/day1.py
@@ -0,0 +1,15 @@
+#open file in \data\ folder
+with open("2022\data\day1.csv", "r") as f:
+    elfList = []
+    currentElf = 0
+    for i, line in enumerate(f):
+        if line.strip():
+            currentElf += int(line.strip())
+        else:
+            elfList.append(currentElf)
+            currentElf = 0
+    partOneSolution = sorted(elfList,reverse=True)[0]
+    partTwoSolution = sum(sorted(elfList,reverse=True)[:3])
+    print("Part 1: " + str(partOneSolution))
+    print("Part 2: " + str(partTwoSolution))
+
